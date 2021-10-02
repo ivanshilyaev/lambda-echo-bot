0413ffc01e65e4e6ccbaac29a0171a7a534b4212
Ivan Shilyaev <shilyaev1433@gmail.com>
2021-10-02 10:51:55 +0300

    Update bot

--- HelloWorldFunction/src/main/java/com/ivanshilyaev/App.java
+++ HelloWorldFunction/src/main/java/com/ivanshilyaev/App.java
@@ -10,27 +10,14 @@ import lombok.extern.slf4j.Slf4j;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 
-import java.io.FileInputStream;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
-import java.util.Properties;
 
 @Slf4j
 public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
-    private String extractToken() throws Exception {
-        String rootPath = Objects.requireNonNull(Thread.currentThread().getContextClassLoader().getResource(""))
-                .getPath();
-        String appConfigPath = rootPath + "app.properties";
-        Properties properties = new Properties();
-        properties.load(new FileInputStream(appConfigPath));
-
-        return properties.getProperty("token");
-    }
-
     private void sendMessage(String text, String chatId) throws Exception {
-        String url = "https://api.telegram.org/bot" + extractToken()
+        String url = "https://api.telegram.org/bot" + System.getenv("token")
                 + "/sendMessage?text=" + text
                 + "&chat_id=" + chatId;
 
